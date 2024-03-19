0fa1c5951ddd0d19fba4d54f5b396ba490a51711
kwondaeho <trewq231@naver.com>
2024-03-19 21:46:03 +0900

    Event domain

---
+++ src/test/java/com/example/restapi/events/EventTest.java
@@ -0,0 +1,35 @@
+package com.example.restapi.events;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class EventTest {
+
+    @Test
+    void builder() {
+        Event event = Event.builder()
+                .name("Infearn Spring REST API")
+                .description("REST API development with Spring")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    void javaBean(){
+        // given
+        String name = "Event";
+        String description = "Spring";
+
+        //when
+        Event event = new Event();
+        event.setName(name);
+        event.setDescription(description);
+
+        //then
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
+
+}
